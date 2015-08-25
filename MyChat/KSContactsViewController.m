4dfeb4f2b05afab070636b06e65709d1346123ab
Fung <fung@itcast.cn>
2015-08-25 17:51:25 +0800

    好友列表显示

--- MyChat/KSContactsViewController.m
+++ MyChat/KSContactsViewController.m
@@ -9,23 +9,59 @@
 #import "KSContactsViewController.h"
 
 @interface KSContactsViewController ()<EMChatManagerDelegate>
-
+/**
+ * 好友数组
+ */
+@property(nonatomic,strong)NSArray *friendsList;
 @end
 
 @implementation KSContactsViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // 1.设置ChatManager代理
     [[KSChatManagerTool sharedTool] addDelegate:self];
+    
+    // 2.获取好友
+    self.friendsList = [[EaseMob sharedInstance].chatManager buddyList];
+    KSLog(@"%@",self.friendsList);
+    
+    // 3.如获本地数据库没联系人，从服务器获取
+    __weak typeof(self) weakSelf = self;
+    if (self.friendsList.count == 0) {
+        [[EaseMob sharedInstance].chatManager asyncFetchBuddyListWithCompletion:^(NSArray *buddyList, EMError *error) {
+            if (error) {
+                KSLog(@"%@",error);
+                [EMAlertView showAlertWithTitle:@"xx" message:error.description];
+            }else{
+                KSLog(@"%@",buddyList);
+                weakSelf.friendsList = buddyList;
+                // 刷新表格
+                [weakSelf.tableView reloadData];
+            }
+            
+        } onQueue:nil];
+    }
+    
 }
 
 
-#pragma mark - Table view data source
+#pragma mark - 表格数据源方法
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return self.friendsList.count;
 }
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    EMBuddy *friend = self.friendsList[indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ContactCell"];
+    
+    cell.textLabel.text = friend.username;
+    
+    return cell;
+}
+
 #pragma mark 发出的好友请求被用户username接受了
 - (void)didAcceptedByBuddy:(NSString *)username{
     NSString *msg = [username stringByAppendingString:@" 用户同意添加你为好友"];
